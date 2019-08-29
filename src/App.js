af2fc9b082fb7afafce1ee5bb0be0f5cdfcea0d8
Mike Slaby <mikeslaby3@gmail.com>
2019-08-29 00:35:51 -0700

    Imported and implemented button component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './pages/Home/Home';
-import HappyBirthday from './pages/HappyBirthday/HappyBirthday';
 
+import Aux from './hoc/Aux';
+import Button from './components/Button/Button';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-          <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/happybirthday" component={HappyBirthday} />
-          </Switch>
-        </Router>
-      </div>
+      <Aux>
+        <Button />
+      </Aux>
     );
   }
 }
