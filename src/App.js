f687552c52c7e10aea13aafe4c9d40b9c2b6534f
Mike Slaby <mikeslaby3@gmail.com>
2019-08-29 12:50:31 -0700

    Added logic for opening and closing modal

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 
 import Aux from './hoc/Aux';
 import Button from './components/Button/Button';
+import Modal from './components/Modal/Modal';
 class App extends Component {
+  state = {
+    showModal: false
+  }
+
+  buttonClickHandler = () => {
+    this.setState({
+        showModal: true
+    });
+  }
+
+  closeModalHandler = () => {
+    this.setState({
+        showModal: false
+    });
+  }
+
   render() {
     return (
       <Aux>
-        <Button />
+        <Button openModal={this.buttonClickHandler} />
+        <Modal show={this.state.showModal} modalClosed={this.closeModalHandler}>
+            <p>Hey Im a Baby</p>
+        </Modal>
       </Aux>
     );
   }
