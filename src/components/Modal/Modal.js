e81d30e9749abca10151dc8eb167c7d548204177
Mike Slaby <mikeslaby3@gmail.com>
2019-08-29 19:26:18 -0700

    Added confetti background image

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,6 +4,8 @@ import classes from './Modal.module.css';
 import Aux from '../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
+const confetti = process.env.PUBLIC_URL + "/images/confetti-background.jpg";
+
 const modal = (props) => (
     <Aux>
         <Backdrop 
@@ -14,7 +16,8 @@ const modal = (props) => (
             className={classes.Modal}
             style={{
                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
+                opacity: props.show ? '1' : '0',
+                backgroundImage: `url(${confetti})`,
             }}>
             {props.children}
         </div>
