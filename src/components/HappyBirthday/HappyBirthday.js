6a86cb576f97e18e61f01afb28d5dc13a8c99410
Mike Slaby <mikeslaby3@gmail.com>
2019-08-29 19:14:37 -0700

    Got rid of confetti background image

--- src/components/HappyBirthday/HappyBirthday.js
+++ src/components/HappyBirthday/HappyBirthday.js
@@ -4,12 +4,10 @@ import classes from './HappyBirthday.module.css';
 
 const happyBirthday = () => {
     return (
-        <div className={classes.Modal}>
-            <img className={classes.Confetti} src={process.env.PUBLIC_URL + "/images/bday-confetti.png"} alt="bday-confetti"></img>
-            <h1 className={[classes.BirthdayFont, classes.glow].join(' ')}>HAPPY BIRTHDAY GABY!</h1>
-            <img className={classes.Gif} src="https://media.giphy.com/media/Dl2seYrwPvfjO/giphy.gif" alt="dancing dog"></img>
-        </div>
-        
+            <div className={classes.Modal}>
+                <h1 className={[classes.BirthdayFont, classes.glow].join(' ')}>HAPPY BIRTHDAY GABY!</h1>
+                <img className={classes.Gif} src="https://media.giphy.com/media/Dl2seYrwPvfjO/giphy.gif" alt="dancing dog"></img>
+            </div>
     )
 }
 
