5a8925a7c6707689b227527e351b0af578960035
Mike Slaby <mikeslaby3@gmail.com>
2019-09-19 19:37:32 -0700

    Added paragraph and styling

--- src/components/GotYourAss/GotYourAss.js
+++ src/components/GotYourAss/GotYourAss.js
@@ -4,9 +4,16 @@ import classes from './GotYourAss.module.css';
 import Hand from '../Hand/Hand';
 
 const gotYourAss = () => {
+
+    const text = {
+        fontSize: '50px',
+        fontFamily: 'Lobster, cursive'
+    }
+
     return (
             <div className={classes.Modal}>
                 <Hand />
+                <p style={text}>Got yo ass!</p>
             </div>
     )
 }
